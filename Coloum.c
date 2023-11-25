ee616550bf413f1dc847158eb50e7e646bd5fc7d
Anirudh joshi <yashjoshi5521@gmail.com>
2023-11-25 17:15:17 +0530

    if-else

---
+++ Coloum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    for(int i = 0;i<=2;i++)
+    {
+        for(int j =0; j<=2;j++)
+        {
+            printf("%d%d ", i , j);
+        }
+        printf("\n");
+    }
+}
---
+++ IGOT.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int i =0;
+    do
+    {
+        printf("finding");
+        do
+        {
+            printf("i got it");
+            i++;
+        } 
+        while (i==53);
+        
+    }
+    
+    while (i<=100);
+    
+}
